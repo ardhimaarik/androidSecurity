bca184627061354c690cd9a9f926f053f2aea048
Ardhi Maarik <ardi@bahasakita.co.id>
2018-01-15 15:14:14 +0700

    add change HideProguard

--- HideStringProgurd/app/src/main/java/com/kirra/hidestringprogurd/MainActivity.java
+++ HideStringProgurd/app/src/main/java/com/kirra/hidestringprogurd/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String data = "Passw0rdRahasi4_3";
+    private final static String data = "Passw0rdRahasi4_code_string";
     private static final byte[] databyte = new byte[]{
-            'P','a','s','s','w','0','r','d','R','a','h','a','s','i','4','_','5'};
+            'P','a','s','s','w','0','r','d','R','a','h','a','s','i','4','_','c','o','d','e','_','b','y','t','e'};
+    private String dataBuildConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataBuildConfig = BuildConfig.pass;
     }
 }
