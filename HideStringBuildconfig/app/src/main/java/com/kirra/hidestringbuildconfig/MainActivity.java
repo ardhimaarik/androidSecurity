13eeb6ef0e247e1aca1605d2d2c9e440ce15f6e7
Ardhi Maarik <ardi@bahasakita.co.id>
2018-01-15 14:28:47 +0700

    add change

--- HideStringBuildconfig/app/src/main/java/com/kirra/hidestringbuildconfig/MainActivity.java
+++ HideStringBuildconfig/app/src/main/java/com/kirra/hidestringbuildconfig/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private String keydata;
+    private String keydata2;
+    private String keydata3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         keydata = BuildConfig.pass;
+        keydata2 = BuildConfig.ENC_KEY_1;
+        keydata3 = BuildConfig.ENC_KEY_IV;
     }
 }
